f8f0a9a54c0f3351b73cadc47ac8708ea364fc80
coashby <8316079+coashby@users.noreply.github.com>
2025-03-25 23:12:19 -0400

    Adding an enzymes library with a simple implementation of RNA polymerase.

---
+++ python/orfaqs/lib/core/enzymes.py
@@ -0,0 +1,45 @@
+'''
+Enzymes
+'''
+
+from orfaqs.lib.core.nucleotides import (
+    DNASequence,
+    NucleicAcid,
+    RNASequence,
+    THYMINE,
+    URACIL
+)
+
+
+class RNAPolymerase:
+    '''RNAPolymerase'''
+    @staticmethod
+    def transcribe(
+            dna_sequence: str | list[str] | list[NucleicAcid] | DNASequence,
+            transcription_start_site: int = 0,
+            termination_sequence: (str |
+                                   list[str] |
+                                   list[NucleicAcid] |
+                                   DNASequence) = None,
+            rna_sequence_name: str = None) -> RNASequence:
+
+        if not isinstance(dna_sequence, DNASequence):
+            dna_sequence = DNASequence(dna_sequence)
+
+        if ((termination_sequence is not None) and
+                (not isinstance(termination_sequence, DNASequence))):
+            termination_sequence = DNASequence(termination_sequence)
+
+        dna_region: DNASequence = dna_sequence[transcription_start_site:]
+
+        termination_site = dna_region.find_sequence(termination_sequence)
+        if termination_site is None:
+            termination_site = dna_region.sequence_length
+
+        dna_region = dna_region[:termination_site]
+
+        rna_sequence_str = dna_region.sequence_str.replace(
+            THYMINE.symbol,
+            URACIL.symbol
+        )
+        return RNASequence(rna_sequence_str, rna_sequence_name)
