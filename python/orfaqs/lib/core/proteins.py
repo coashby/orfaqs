0fc4ba9054b5ed2101b14704bc0bd27172792c90
coashby <8316079+coashby@users.noreply.github.com>
2025-03-25 23:07:32 -0400

    Adding an abstraction for proteins.

---
+++ python/orfaqs/lib/core/proteins.py
@@ -0,0 +1,46 @@
+'''
+Proteins
+'''
+from orfaqs.lib.core.aminoacids import AminoAcid
+
+
+class Protein:
+    '''Protein'''
+
+    def __init__(self,
+                 amino_acid: AminoAcid | list[AminoAcid] = None,
+                 protein_name: str = None):
+        if amino_acid is None:
+            amino_acid = []
+        elif isinstance(amino_acid, AminoAcid):
+            amino_acid = [amino_acid]
+
+        self._protein_name = protein_name
+        self._amino_acid_chain = amino_acid
+        self._sequence_complete = False
+
+    def __str__(self) -> str:
+        return ''.join(
+            [amino_acid.symbol for amino_acid in self._amino_acid_chain]
+        )
+
+    @property
+    def protein_name(self):
+        return self._protein_name
+
+    @property
+    def amino_acid_chain(self) -> list[AminoAcid]:
+        return self._amino_acid_chain
+
+    @property
+    def sequence_complete(self) -> bool:
+        return self._sequence_complete
+
+    def add_amino_acid(self, amino_acid: AminoAcid):
+        if self._sequence_complete:
+            return
+
+        self._amino_acid_chain.append(amino_acid)
+
+    def end_sequence(self):
+        self._sequence_complete = True
