dd8ab50d9a1f077096a4e4a17718fc79ef711fe9
coashby <8316079+coashby@users.noreply.github.com>
2025-05-31 00:39:14 -0400

    Adding new test class for CodonUtils.

---
+++ python/orfaqs/tests/lib/core/test_codons.py
@@ -0,0 +1,11 @@
+import orfaqs.lib.core.codons as codons
+
+
+class TestCodonUtils:
+    _codon_utils = codons.CodonUtils()
+
+    def test_available_codons(self):
+        pass
+
+    def test_base_triplet_to_codon(self):
+        pass
