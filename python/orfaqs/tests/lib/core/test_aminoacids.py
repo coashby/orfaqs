2a0dc4e10d522e7794cccadd7dd382828eff41d1
coashby <8316079+coashby@users.noreply.github.com>
2025-06-02 14:17:51 -0400

    [bugfix] Removing runtime errors in test files. Adding pytest-datadir plugin to support reference data.

--- python/orfaqs/tests/lib/core/test_aminoacids.py
+++ python/orfaqs/tests/lib/core/test_aminoacids.py
@@ -1,24 +1,31 @@
 import orfaqs.lib.core.aminoacids as aminoacids
 import pandas as pd
+import pytest
+
+
+@pytest.fixture
+def reference_amino_acids(shared_datadir) -> dict[str, dict]:
+    reference_amino_acids_file_path = (
+        shared_datadir / 'reference_amino_acids.csv'
+    )
+    reference_amino_acids_dataframe = pd.read_csv(
+        reference_amino_acids_file_path
+    )
+
+    reference_amino_acids_dataframe = (
+        reference_amino_acids_dataframe.set_index('name')
+    )
+    amino_acids: dict[str, dict] = {}
+    for amino_acid_name, row in reference_amino_acids_dataframe.iterrows():
+        amino_acids[amino_acid_name] = dict(row)
+
+    return amino_acids
 
 
 class TestAminoAcidsUtils:
     _amino_acid_utils = aminoacids.AminoAcidUtils()
 
-    def test_available_amino_acids(self):
-        reference_amino_acids_file_path = (
-            './orfaqs/tests/resources/reference_amino_acids.csv'
-        )
-        reference_amino_acids_dataframe = pd.read_csv(
-            reference_amino_acids_file_path
-        )
-        reference_amino_acids_dataframe = (
-            reference_amino_acids_dataframe.set_index('name')
-        )
-        reference_amino_acids: dict[str, dict] = {}
-        for amino_acid_name, row in reference_amino_acids_dataframe.iterrows():
-            reference_amino_acids[amino_acid_name] = dict(row)
-
+    def test_available_amino_acids(self, reference_amino_acids):
         available_amino_acids = self._amino_acid_utils.available_amino_acids()
         assert len(reference_amino_acids) == len(available_amino_acids)
         for amino_acid in available_amino_acids:
