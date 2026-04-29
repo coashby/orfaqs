19f3939ba17a263e3cc0a80f77b46ea7d3bac9b9
coashby <8316079+coashby@users.noreply.github.com>
2026-04-29 10:14:56 -0400

    Adding missing orfaqsproteinquery/databases modules.

---
+++ orfaqs/modules/python/orfaqsproteinquery/databases/orfaqsproteintableutils.py
@@ -0,0 +1,49 @@
+import enum
+import logging
+
+
+from orfaqs.lib.python.core.nucleotides import NucleotideUtils
+
+_logger = logging.getLogger(__name__)
+
+
+class ORFaqsProteinTableUtils:
+    @staticmethod
+    def uid_comment() -> str:
+        return 'The unique id of the entry.'
+
+    @staticmethod
+    def source_uid_comment() -> str:
+        return (
+            'The identifier assigned to the sequence from '
+            'which the protein was translated.'
+        )
+
+    @staticmethod
+    def strand_type_comment() -> str:
+        strand_types_str = NucleotideUtils.available_strand_types_str()
+        return f'The genomic sequence strand {strand_types_str} used during translation.'
+
+    @staticmethod
+    def reading_frame_comment() -> str:
+        return 'The reading frame (1, 2, or 3) from which the protein was transcribed.'
+
+    @staticmethod
+    def rna_sequence_position_comment() -> str:
+        return (
+            'The position of the start codon in the RNA sequence '
+            'from which the protein was transcribed.'
+        )
+
+    @staticmethod
+    def protein_comment() -> str:
+        return 'The protein represented by its single-letter amino acid abbreviations.'
+
+    @staticmethod
+    def protein_length_comment() -> str:
+        return 'The number of amino acids in the protein sequence.'
+
+
+class ORFaqsProteinTableType(enum.Enum):
+    DISCOVERED_PROTEINS = enum.auto()
+    REFERENCE_PROTEINS = enum.auto()
